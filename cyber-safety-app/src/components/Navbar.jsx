9b914275559154161bc02fabaf07aca103eeedba
Lukeroxburgh <71620670+Lukeroxburgh@users.noreply.github.com>
2023-10-05 13:40:00 +1000

    nav bar

--- cyber-safety-app/src/components/Navbar.jsx
+++ cyber-safety-app/src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className=''>
-            
+        <div className='w-screen h- [80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
+            <div className='px-2 flex justify-between items-center w-full h-full'>
+                <div className='flex items-center'>
+                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>CYBER CENTRE</h1>
+                </div>
+            </div>
+            <MenuIcon className='W-5' />
         </div>
     )
 }
